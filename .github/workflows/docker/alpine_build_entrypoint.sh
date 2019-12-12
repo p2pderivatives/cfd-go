9c0b65459a998b58ce93732d4d3bb0d3317206c0
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2019-12-12 10:12:36 +0900

    add release&upload action from tag

---
+++ .github/workflows/docker/alpine_build_entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+apk add --update --no-cache musl gcc g++ make git cmake
+
+cd /github/workspace
+ls
+
+mkdir dist
+cd cfd-go
+mkdir build
+cd build
+cmake --version
+cmake .. -DENABLE_SHARED=on -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTS=off -DENABLE_JS_WRAPPER=off -DENABLE_CAPI=on -DTARGET_RPATH="/usr/local/lib;/usr/local/lib64"
+make
+make install DESTDIR=/github/workspace/dist
+ls /github/workspace/dist
+ls /github/workspace/dist/usr
+ls /github/workspace/dist/usr/local
+ls /github/workspace/dist/usr/local/lib64
